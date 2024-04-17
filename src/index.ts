3f78372a55b4c319b69c681922a14419cfca4773
Fabio Colella <fcole90@gmail.com>
2024-04-17 21:23:18 +0200

    Add initial communication layers

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ const io = new Server({ /* options */ });
 console.log('Lobby server ready')
 io.on("connection", (socket) => {
   console.log("Connected to:", socket.id); // x8WIv7-mJelg7on_ALbx
-});
+  
+  socket.emit("handshake", socket.id)
+  
+  socket.on("broadcastMessage", (message) => {
+    socket.emit("broadcastMessage", socket.id, message)
+  })
+})
+
+
 
 io.listen(3000);
