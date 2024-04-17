faf4521841f2d7759ae21eb75c6a984f03886e10
Fabio Colella <fcole90@gmail.com>
2024-04-17 20:09:10 +0200

    Complete first connection

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { Server } from "socket.io";
 
 const io = new Server({ /* options */ });
 
-console.log('NO')
+console.log('Lobby server ready')
 io.on("connection", (socket) => {
-  // ...
+  console.log("Connected to:", socket.id); // x8WIv7-mJelg7on_ALbx
 });
 
 io.listen(3000);
